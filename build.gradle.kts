cb637450dfddd2dcd643a52d228b3c2b77dce44a
Vladimir <nonobodysfault@gmail.com>
2018-01-02 11:17:48 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application") version "3.0.1"
+    kotlin("android") version "1.2.10"
+    kotlin("android.extensions") version "1.2.10"
+}
+
+android {
+    buildToolsVersion("27.0.0")
+    compileSdkVersion(27)
+
+    defaultConfig {
+        minSdkVersion(15)
+        targetSdkVersion(27)
+
+        applicationId = "com.example.kotlingradle"
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles("proguard-rules.pro")
+        }
+    }
+}
+
+androidExtensions {
+    isExperimental = true
+}
+
+dependencies {
+    compile("com.android.support:appcompat-v7:27.0.0")
+    compile("com.android.support.constraint:constraint-layout:1.0.2")
+    compile(kotlin("stdlib", "1.2.10"))
+}
+
+repositories {
+    jcenter()
+    google()
+}
---
+++ src/main/kotlin/com/example/kotlingradle/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotlingradle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.extensions.LayoutContainer
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
